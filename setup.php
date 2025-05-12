803858ac62cc4352e7ec5064c2a7dc0ceb8619a9
edison-infotel <edison.jace@infotel.com>
2025-05-12 08:30:03 +0200

    WEBDIR fix

--- setup.php
+++ setup.php
@@ -36,8 +36,8 @@ define('PLUGIN_RELEASES_VERSION', '2.0.4');
 if (!defined("PLUGIN_RELEASES_DIR")) {
    define("PLUGIN_RELEASES_DIR", Plugin::getPhpDir("releases"));
    define("PLUGIN_RELEASES_NOTFULL_DIR", Plugin::getPhpDir("releases",false));
-   define("PLUGIN_RELEASES_WEBDIR", '/plugins/releases');
-   define("PLUGIN_RELEASES_NOTFULL_WEBDIR", $CFG_GLPI['root_doc'] . '/plugins/releases');
+   define("PLUGIN_RELEASES_WEBDIR", $CFG_GLPI['root_doc'] . '/plugins/releases');
+   define("PLUGIN_RELEASES_NOTFULL_WEBDIR", '/plugins/releases');
 }
 
 // Init the hooks of the plugins -Needed
