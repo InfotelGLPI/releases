2f78ae4685036ebfe995b0e50c6b9239af641b07
Thetsmr <thetsmr@gmail.com>
2025-07-07 16:45:11 +0200

    Fix create & ediditing followup

--- inc/release.class.php
+++ inc/release.class.php
@@ -3767,7 +3767,7 @@ class PluginReleasesRelease extends CommonITILObject
 
     public static function getTaskClass()
     {
-        // TODO: Implement getTaskClass() method.
+        return PluginReleasesDeploytask::class;
     }
 
     public static function getContentTemplatesParametersClass(): string
