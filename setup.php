d69adc96dd8c0c47d67cd4d7ce6098a7ada48fdf
Thetsmr <thetsmr@gmail.com>
2026-08-19 16:05:32 +0200

    Bump version

--- setup.php
+++ setup.php
@@ -37,7 +37,7 @@ use GlpiPlugin\Releases\Profile;
 use GlpiPlugin\Releases\Release;
 use GlpiPlugin\Releases\Release_Item;
 
-define('PLUGIN_RELEASES_VERSION', '2.2.0');
+define('PLUGIN_RELEASES_VERSION', '2.2.1');
 
 if (!defined("PLUGIN_RELEASES_DIR")) {
     define("PLUGIN_RELEASES_DIR", Plugin::getPhpDir("releases"));
